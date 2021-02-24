5e812e073879040ce0f4525d4dbdedbf53c85673
venkat102 <venkatesharunachalam659@gmail.com>
2021-02-24 23:41:09 +0530

    feat: auto populate data

--- textile/textile/doctype/sales_invoice/sales_invoice.js
+++ textile/textile/doctype/sales_invoice/sales_invoice.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Sales Invoice', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm){
+		frappe.call({
+			method:"textile.textile.doctype.sales_invoice.sales_invoice.sales_invoice_count",
+			callback: function(res){
+				frm.set_value("sales_invoice_number", res.message[0][0]);
+				frm.set_value("date", res.message[1]);
+				frm.refresh();
+			}
+		})
+	},
+	customer:function(frm){
+		if(frm.doc.customer){
+			frappe.call({
+				method:"textile.textile.doctype.sales_invoice.sales_invoice.customer_count",
+				args:{
+					'customer': frm.doc.customer
+				},
+				callback: function(res){
+					frm.set_value("customer_invoice_number", res.message[0][0]);
+					frm.refresh();
+				}
+			})
+		}
+	}
 });
--- textile/textile/doctype/sales_invoice/sales_invoice.py
+++ textile/textile/doctype/sales_invoice/sales_invoice.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+@frappe.whitelist()
+def sales_invoice_count():
+	return [frappe.db.sql('''SELECT COUNT(name)+1 FROM `tabSales Invoice`'''), frappe.utils.today()]
+
+@frappe.whitelist()
+def customer_count(customer):
+	return frappe.db.sql(f'''SELECT COUNT(name)+1 FROM `tabSales Invoice` WHERE customer=\'{customer}\' ''')
 
 class SalesInvoice(Document):
 	pass
